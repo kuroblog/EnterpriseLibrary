734387bc76290907731dedf372ba7a5520d1e340
Kuro <kuro_q@live.com>
2016-10-21 11:54:23 +0800

    create ConfigurationBlock solution

---
+++ SourceCode/ConfigurationBlock/ConfigurationBlock.Basic/Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace ConfigurationBlock.Basic
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
