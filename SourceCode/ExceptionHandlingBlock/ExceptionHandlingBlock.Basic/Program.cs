4d9db6b420a98956416b40578e774fcbfa56ea5d
Kuro <kuro_q@live.com>
2016-10-21 10:15:01 +0800

    implement packing exception

--- SourceCode/ExceptionHandlingBlock/ExceptionHandlingBlock.Basic/Program.cs
+++ SourceCode/ExceptionHandlingBlock/ExceptionHandlingBlock.Basic/Program.cs
@@ -4,6 +4,7 @@ namespace ExceptionHandlingBlock.Basic
     using Microsoft.Practices.EnterpriseLibrary.Common.Configuration;
     using Microsoft.Practices.EnterpriseLibrary.ExceptionHandling;
     using System;
+    using System.Collections;
     using System.Collections.Generic;
     using System.IO;
 
@@ -30,7 +31,7 @@ namespace ExceptionHandlingBlock.Basic
                 var index = 0;
                 do
                 {
-                    errors.Add(Tuple.Create(++index, tmp.GetType().Name, tmp.Message.Replace(Environment.NewLine, string.Empty)));
+                    errors.Add(Tuple.Create(++index, ex.GetType().Name, ex.Message.Replace(Environment.NewLine, string.Empty)));
                     ex = tmp.InnerException == null ? null : ex.InnerException;
                 } while (ex != null);
 
@@ -55,12 +56,13 @@ namespace ExceptionHandlingBlock.Basic
             ExceptionPolicy.SetExceptionManager(exceptionManager);
             #endregion
 
-            var isException = false;
+            var exceptions = new List<bool>();
 
-            isException = ProcessException(exceptionManager, () => int.Parse("A"));
-            isException = ProcessException(exceptionManager, () => File.Open("tmp.txt", FileMode.Open));
+            exceptions.Add(ProcessException(exceptionManager, () => int.Parse("A")));
+            exceptions.Add(ProcessException(exceptionManager, () => File.Open("tmp.txt", FileMode.Open)));
+            exceptions.Add(ProcessException(exceptionManager, () => new Hashtable()["tmp"].ToString()));
 
-            Console.WriteLine("主函数执行完成，{0}异常发生，按任意键退出……", isException ? "有" : "无");
+            Console.WriteLine("主函数执行完成，{0}异常发生，按任意键退出……", exceptions.Count > 0 ? "有" : "无");
             Console.ReadKey();
         }
     }
